0cfd3e2ea10fe62d0d4c58c6131e4cef6c4d993b
JLuis Avalos <luizavalos40@gmail.com>
2023-05-30 11:25:05 -0500

    selecting cards works

--- src/components/Breeds.tsx
+++ src/components/Breeds.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useDogsSearch from "../hooks/useDogsSearch";
 import DogCard from "./DogCard";
 import Pagination from "./Pagination";
@@ -6,14 +7,32 @@ import Spinner from "./Spinner";
 
 function Breeds() {
   const { dogs, isLoading } = useDogsSearch();
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const handleSelect = (id: string) => {
+    setSelected((prev) => [id, ...prev]);
+  };
+
+  const handleUnselect = (id: string) => {
+    setSelected((prev) => prev.filter((item) => item !== id));
+  };
 
   return (
     <div className="w-full">
+      {selected.length}
       {isLoading ? <Spinner /> : null}
       <SortingMenu />
       <div className="flex flex-row gap-4 mx-auto flex-wrap justify-center">
         {dogs.map((dog) => {
-          return <DogCard key={dog.id} dog={dog} />;
+          return (
+            <DogCard
+              selectedItems={selected}
+              key={dog.id}
+              dog={dog}
+              handleUnselect={() => handleUnselect(dog.id)}
+              handleSelect={() => handleSelect(dog.id)}
+            />
+          );
         })}
       </div>
       <Pagination />
--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -2,11 +2,22 @@ import { Dog } from "../hooks/useDogsSearch";
 
 function DogCard({
   dog: { id, name, breed, age, zip_code, img },
-}: {
-  dog: Dog;
-}) {
+  handleSelect,
+  handleUnselect,
+  selectedItems,
+}: DogCardProps) {
+  const isSelected = selectedItems.includes(id);
+
+  const handleToggleSelect = () => {
+    if (isSelected) {
+      handleUnselect(id);
+    } else {
+      handleSelect(id);
+    }
+  };
+
   return (
-    <div key={id} className="p-4 shadow-md shadow-slate-500">
+    <div key={id} className="p-4 shadow-md shadow-slate-500 relative">
       <h2 className="text-3xl">{name}</h2>
       <h3 className="text-xl text-slate-700">{breed}</h3>
       <img
@@ -16,12 +27,28 @@ function DogCard({
         alt={`image of a ${breed} named ${name}`}
       />
 
-      <div className=" flex flex-col align-bottom justify-end">
+      <div className=" flex flex-col align-bottom justify-end mb-5">
         <p className="text-slate-700 font-semibold">Age: {age}</p>
         <p>location: {zip_code}</p>
       </div>
+      <button
+        onClick={handleToggleSelect}
+        className={`${
+          isSelected ? "bg-slate-200 text-black" : "bg-black text-white"
+        } w-full absolute bottom-0 left-0 right-0`}
+      >
+        {isSelected ? "Unselect" : "Select"}
+      </button>
     </div>
   );
 }
 
 export default DogCard;
+
+type DogCardProps = {
+  dog: Dog;
+  handleUnselect: (id: string) => void;
+
+  handleSelect: (id: string) => void;
+  selectedItems: string[];
+};
